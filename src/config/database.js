bf5b005023292ea1e49b50d1bac3b8e0ff1b264c
Erick Leandro <j.erickleandro@gmail.com>
2021-05-06 17:54:08 -0300

    modificações no teste

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,14 @@
 require('dotenv').config({
-  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+  path: process.env.NODE_ENV.toString().trim() == "test" ? ".env.test" : ".env"
 })
-
-module.exports = {
+console.log(process.env.NODE_ENV.toString().trim() == "test");
+module.exports = 
+{
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  dialect: process.env.DB_DIALECT || "postgres",
+  dialect: process.env.DB_DIALECT ||"postgres",
   storage: './__tests__/database.sqlite',
   operatorsAliases: false,
   logging: false,
