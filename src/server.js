39d17584c509e412c063c26d186c3de76afd5d02
Victor Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-05-08 19:22:34 -0300

    feat: firulagens

--- src/server.js
+++ src/server.js
@@ -1,4 +1,16 @@
 const app = require('./app');
+const chalk = require('chalk')
+const os = require('os')
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  const arch = os.arch()
+  const plataform = os.platform()
+  const type = os.type()
+  const mem = os.totalmem()
+  const cpus = os.cpus()
+  console.log(chalk.bgCyan.black(`SO: ${type} ${plataform} ${arch}`))
+  console.log(chalk.bgCyan.black(`RAM: ${Math.floor(mem * (10 ** -9))} GB`))
+  console.log(chalk.bgCyan.black(`CORES: ${cpus.length}`))
+  console.log(chalk.bgCyan.black(`CPU: ${cpus[0].model}`))
+});
 
