975dff59764c7b0acde04b9e02dc2423fdee1b51
Victor Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-05-08 18:16:43 -0300

    feat[customer]: add update routine to CustomerController

--- src/controller/CustomerController.js
+++ src/controller/CustomerController.js
@@ -104,7 +104,83 @@ function post(req, res) {
   })
 }
 
+/**
+ * Atualiza dados de um Customer
+ * 
+ * @method update
+ * @param {Request} req objeto Request da requisição
+ * @param {Response} res objeto Response da requisição
+ * @returns {Customer}
+ */
+function update(req, res) {
+  /**
+   * Dados do body da requisição
+   * 
+   * @property {String} name nome do Customer
+   * @property {String} email email do Customer
+   * @property {String} govId cpf do Customer
+   * @property {String} birthdate (yyyy-mm-dd)
+   */
+  let {
+    name,
+    email,
+    govId,
+    birthdate
+  } = req.body
+  
+  /**
+   * Id do Customer que sofrerá a mudança
+   * 
+   * @param {String} id
+   */
+  const { id } = req.params
+
+  /**
+   * Remove espaços à esquerda e à direita de name
+   */
+   name = name.trim()
+
+   if (!validateName(name)) {
+     log(chalk.bgRed("ERRO: Name invalid => USER: ", name))
+     return res.status(422).send("Formato de Nome invalido")
+   }
+ 
+   if (!validateCPF(govId)) {
+     log(chalk.bgRed("ERRO: CPF invalid => USER: ", name, " CPF: ", govId))
+     return res.status(422).send("Formato de CPF invalido")
+   }
+   
+   if(!validateEmail(email)) {
+     log(chalk.bgRed("ERRO: EMAIL invalid => USER: ", name, " EMAIL: ", email))
+     return res.status(422).send("Formato de Email invalido")
+   }
+   
+   if(!validateDate(birthdate)) {
+     log(chalk.bgRed("ERRO: Date invalid => USER: ", name, " Date: ", birthdate))
+     return res.status(422).send("Formato de Data invalido")
+   }
+
+   Customer.update({
+     name: name,
+     email: email,
+     govId: govId,
+     birthdate: birthdate
+   },{
+     where: {
+       id: id
+     }
+   })
+   .then(resp => {
+    res.status(200).json(resp).send()
+    })
+    .catch(err => {
+      log(chalk.bgRed("ERRO: ", err))
+      res.status(500).send("erro interno, tente novamente em alguns instantes")
+    })
+}
+
 module.exports.CustomerController = {
   get,
-  post
+  post,
+  update
 }
