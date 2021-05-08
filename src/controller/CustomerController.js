a6434f779ae83f79906bb672738335fc28944fc2
Victor Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-05-08 17:22:55 -0300

    feat[controller]: add chalk logging

--- src/controller/CustomerController.js
+++ src/controller/CustomerController.js
@@ -1,7 +1,8 @@
 const { Customer } = require("../app/models/Customer");
 const validateCPF = require("../utils/cpf");
 const validateEmail = require("../utils/email");
-const chalk = require('chalk')
+const chalk = require('chalk');
+const validateName = require("../utils/name");
 
 const log = console.log.bind(console)
 
@@ -24,33 +25,43 @@ function get(req, res) {
 }
 
 function post(req, res) {
-  const {
+  let {
     name,
     email,
     govId,
     birthdate
   } = req.body
 
+  name = name.trim()
+
+  if (!validateName(name)) {
+    log(chalk.bgRed("ERRO: Name invalid => USER: ", name))
+    return res.status(422).send("Formato de Nome invalido")
+  }
+
   if (!validateCPF(govId)) {
     log(chalk.bgRed("ERRO: CPF invalid => USER: ", name, " CPF: ", govId))
     return res.status(422).send("Formato de CPF invalido")
   }
-
-  if(!validateEmail(email))
+  
+  if(!validateEmail(email)) {
+    log(chalk.bgRed("ERRO: EMAIL invalid => USER: ", name, " EMAIL: ", email))
     return res.status(422).send("Formato de Email invalido")
-
+  }
+  
   Customer.create({
     name: name,
     email: email,
     govId: govId,
     birthdate: birthdate
   })
-    .then(resp => {
-      res.status(200).json(resp).send()
-    })
-    .catch(err => {
-      res.status(400).json(err).send()
-    })
+  .then(resp => {
+    res.status(200).json(resp).send()
+  })
+  .catch(err => {
+    log(chalk.bgRed("ERRO: ", err))
+    res.status(500).send("erro interno, tente novamente em alguns instantes")
+  })
 }
 
 module.exports.CustomerController = {
